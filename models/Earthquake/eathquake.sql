9c935f830197f98ccc7934533924d1d31366f72c
Muhammad Anas <m.anas.dataanalyst109@gmail.com>
2026-07-02 08:22:58 +0000

    solve error problem like AM05 and 06 and update yml , also solve case sensitive issue of colum magType to magtype

--- models/Earthquake/eathquake.sql
+++ models/Earthquake/eathquake.sql
@@ -9,7 +9,7 @@ cleaned_eq as (
         cast(id as string) as earthquake_id,
         cast(properties_type as string) as event_type,
         cast(properties_mag as float64) as magnitude,
-        cast(properties_magType as string) as magnitude_type,
+        cast(properties_magtype as string) as magnitude_type,
         cast(properties_place as string) as place_description,
         cast(properties_title as string) as event_title,
         cast(properties_status as string) as review_status,
